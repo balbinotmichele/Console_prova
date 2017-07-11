5f6bf304ca9016c47dd0261c4304969f605b0426
Michele Balbinot <michele.balbinot@marconirovereto.it>
2017-07-11 13:07:58 +0200

    added edit and save buttons

--- src/pages/components/insegnanti-edit-component/insegnanti-edit.component.ts
+++ src/pages/components/insegnanti-edit-component/insegnanti-edit.component.ts
@@ -1,16 +1,29 @@
 import { School } from './../../../app/Classes/school';
 import { WebService } from './../../../app/WebService';
 import { Teacher } from './../../../app/Classes/teacher';
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'insegnanti-edit',
-  templateUrl: "insegnanti-edit-component.html"
+  templateUrl: "insegnanti-edit-component.html",
+  styles: [`
+    input:disabled, ion-select:disabled, .select-disabled, .item-select-disabled ion-label {
+      opacity:1;
+    }
+  `]
 })
 
 export class InsegnanteEditComponent {
   @Input() teacher: Teacher;
   @Input() sections : string[];
+  @Input() schoolId : string;
+  school : School;
+  edit:boolean = false;
+  @Input() newTeacher : boolean;
 
   constructor(private webService : WebService) {}
+
+  onSaveTeacher() {
+    this.webService.addTeacher(this.schoolId, this.teacher).then(tmp => {console.log(tmp); this.school = tmp; this.teacher = tmp.teachers[tmp.teachers.length - 1];});
+  }
 }
